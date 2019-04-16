cd943330370fe65e362b3fc8ab56520414b105d2
Tomasz Krzyżak <tokrzyzak@outlook.com>
2019-04-16 14:16:38 +0200

    Dodaj pliki projektów.

---
+++ Dziennik/Diary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik
+{
+    class Diary
+    {
+        //zmienne
+        List<float> ratings = new List<float>();
+
+        //Metody
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalcAverage()
+        {
+            float suma = 0, srednia = 0;
+            foreach (var item in ratings)
+            {
+                suma += item;
+            }
+            srednia = suma / ratings.Count();
+
+            return srednia;
+        }
+
+        public float GetMax()
+        {
+            return ratings.Max();
+        }
+
+        public float GetMin()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ Dziennik/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+
+            diary.AddRating(5);
+            diary.AddRating(8.5f);
+            diary.AddRating(4.7f);
+
+            float avg = diary.CalcAverage();
+            float max = diary.GetMax();
+            float min = diary.GetMin();
+
+            for (;  ; )
+            {
+                Console.WriteLine("Podaj liczbe");
+
+                float rating = float.Parse(Console.ReadLine());
+               
+                //warunek wyjscia z petli
+                if (rating == 11)
+                {
+                    break;
+                }
+
+                diary.AddRating(rating);
+            }
+            Console.WriteLine("srednia ocen to " + diary.CalcAverage());
+            Console.WriteLine("MAX:" + diary.GetMax());
+            Console.WriteLine("MIN:" + diary.GetMin());
+            Console.ReadKey();
+        }
+    }
+}
